6ecb5db5362aafd89d01473b7b439a1df55fb13f
casmonido <casmonido@gmail.com>
2017-05-29 16:55:47 +0200

    rev() executes in a loop

--- main.py
+++ main.py
@@ -1,53 +1,61 @@
 #!/usr/bin/python2
 from struct import *
 import socket
+import struct
 import sys
 import mutex
 import threading
 import time
 import logging
 import SLMP
-import StringIO
 import linecache
+import errno
+from socket import error as socket_error
 
-#zawartosc pliku konfiguracyjnego, nadawane funkcja configure
-global SCADA_IP  
-global SCADA_PORT 
-global BUFFER_SIZE  # sa rozne dla scady i servera - do zmiany
-global PLC_SERVER_PORT 
-
-#mutexy na ktorych zawieszac sie beda serwer i klient
+# zmienne do synchronizacji
 waitngForMessage = threading.Semaphore(0)  
 waitingForResponse = threading.Semaphore(0) 
+notEmpty = threading.Condition()
+occupied = 0 
+global scadaMessage
+global serverReply
+# zmienne ustawiane przy konfiguracji srodowiska
+global SCADA_IP  
+global SCADA_PORT 
+global PLC_SERVER_PORT
 
-notEmpty = threading.Condition() #wait(), notify()
-occupied = 0
 
 
-scadaMessage = ""
-serverReply = ""
-
-def configure():
-	global SCADA_IP  # docelowo do pliku konfiguracyjnego 
-	global SCADA_PORT 
-	global BUFFER_SIZE  # sa rozne dla scady i servera - do zmiany
+def configure(configFilePath):
+	global SCADA_IP, SCADA_PORT 
 	global PLC_SERVER_PORT 
-	configureFile = open("config.txt")
-	#pobieramy linie
-	SCADA_IP =linecache.getline("config.txt" , 1) 
-	## sa w formacie string bierzemy wszystko od 10 znaku do przedostatniego
-	SCADA_IP = SCADA_IP[10:-1]					
-	SCADA_PORT =linecache.getline("config.txt" , 2)
+	global SCADA_BUFFER_SIZE, SERVER_BUFFER_SIZE
+
+	configFile = open(configFilePath)
+
+	SCADA_IP = linecache.getline(configFilePath, 1) # pobierz linie
+	SCADA_IP = SCADA_IP[10:-1] # znajac format linii: 'SCADA_IP: 127.0.0.1', pobierz znaki od 10-go do przedostaniego
+	SCADA_PORT = linecache.getline(configFilePath, 2)
 	SCADA_PORT = SCADA_PORT[12:-1]
 	SCADA_PORT = int(SCADA_PORT)
-	BUFFER_SIZE =linecache.getline("config.txt", 3)
-	BUFFER_SIZE = BUFFER_SIZE[13:-1]
-	BUFFER_SIZE = int(BUFFER_SIZE)  ##wymaga dodatkowego rzutowania, chcemy inta a nie stringa
-	PLC_SERVER_PORT =linecache.getline("config.txt" , 4)
+	PLC_SERVER_PORT =linecache.getline(configFilePath, 3)
 	PLC_SERVER_PORT = PLC_SERVER_PORT[17:-1]
 	PLC_SERVER_PORT = int(PLC_SERVER_PORT)
+	configFile.close()
 
-	configureFile.close()
+
+
+# funkcja pomocnicza do odbierania zadanej liczby bajtow za pomaca recv()
+def recvall(sock, expectedLen):
+	data = ''
+	while len(data) < expectedLen:
+		packet = sock.recv(expectedLen - len(data)) # recv() pobierze maksimum tyle bajtow, ile podane w argumencie
+		if not packet:
+			return None
+		data += packet
+	return data
+
+
 
 class ServerThread (threading.Thread):
 
@@ -57,21 +65,34 @@ class ServerThread (threading.Thread):
 		self.port = port
 
 	def run(self):
-
 		global scadaMessage, serverReply
 		ServerSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		ServerSock.connect((self.ip, self.port))
-		logging.debug('ServerThread connected')
-		
-		while 1:
+		# w razie braku polaczenia z serwerem ponawiaj proby
+		while True:
+			try:
+				connected = ServerSock.connect((self.ip, self.port))
+			except socket_error as serr:
+				if serr.errno != errno.ECONNREFUSED:
+					raise serr # to nie jest blad ktory chcemy obsluzyc - re-raise
+				print('[ServerThread]\t Waiting for connection to the server...')
+				time.sleep(5)
+				continue # sprobuj ponownie
+			else: # jesli nie bylo except, idz dalej
+				break
 
+		print('[ServerThread]\t Connected')
+		while 1:
 			waitngForMessage.acquire()
 
-			ServerSock.send(scadaMessage) 
-			myServerReply = ServerSock.recv(BUFFER_SIZE)
-			logging.debug(SLMP.binary_array2string(myServerReply))
+			ServerSock.send(scadaMessage)
 
+			myServerReply = recvall(ServerSock, 9) # to find out why 9, see SMLP manual page 23
+			msgLen = struct.unpack('<H', myServerReply[7:]) # <H means litle endian ushort
+			myServerReply += recvall(ServerSock, msgLen[0]) # msgLen is a tuple
+
+			logging.debug(SLMP.binary_array2string(myServerReply))
 			serverReply = myServerReply
+
 			waitingForResponse.release()
 
 
@@ -83,60 +104,61 @@ class ClientThread (threading.Thread):
 		self.ClientSock = ClientSock
 
 	def run(self):
-
 		global occupied, scadaMessage, serverReply
 		#while 1:
-		logging.debug('ClientThread receiving data...')
-		myScadaMessage = self.ClientSock.recv(BUFFER_SIZE) # tak chyba nie mozna do StringIO
-
+ 
+		myScadaMessage = recvall(self.ClientSock, 9) 
+		print('[ClientThread]\t Receiving message')
+		msgLen = struct.unpack('<H', myScadaMessage[7:])
+		myScadaMessage += recvall(self.ClientSock, msgLen[0]) 
+		logging.debug(SLMP.binary_array2string(myScadaMessage))
 
 		notEmpty.acquire()
 		while occupied == 1:
 			notEmpty.wait()
 		occupied = 1
-
 		notEmpty.release()
 
 		scadaMessage = myScadaMessage
 
 		waitngForMessage.release()
-		logging.debug('Client waiting for response')
-		time.sleep(5)
+		print('[ClientThread]\t Waiting for response from server')
+		time.sleep(2)
+
 		waitingForResponse.acquire()
 		myServerReply = serverReply
-		logging.debug ('Client wake up')
+
 		notEmpty.acquire()
 		occupied = 0
-		
-		self.ClientSock.send(myServerReply)
-		logging.debug('To Client Sent message')			
 		notEmpty.notify()
 		notEmpty.release()
-			
-		logging.debug('Exiting Client Thread')
-		self.ClientSock.close()
+
+		self.ClientSock.send(myServerReply)
+		print('[ClientThread]\t Reply sent to SCADA')
 
 
 
-#main
-configure()
 
+# konfiguracja srodowiska: 
+if  len(sys.argv) < 2:
+	print("Przy wywolywaniu programu, podaj sciezke do pliku konfiguracyjnego")
+	quit()
+configure(sys.argv[1])
 logging.basicConfig(level=logging.DEBUG,format='[%(levelname)s] (%(threadName)-9s) %(message)s',)
 
-client_threads_collection = []
+# tworzenie watkow:
+serverThread = ServerThread('127.0.0.1', PLC_SERVER_PORT)
+serverThread.daemon = True		# watek zostanie zamkniety/umrze kiedy zginie watek glowny
+serverThread.start()
 
-serverthread = ServerThread('127.0.0.1', PLC_SERVER_PORT)
-serverthread.daemon = True   ##to znaczy ze watek zostanie zamkniety/umrze jak zginie watek glowny
-serverthread.start()
-
+clientThreadsCollection = []
 ClientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ClientSock.bind((SCADA_IP, SCADA_PORT))
 ClientSock.listen(1)
+print("Proxy oczekuje polaczen na porcie " + str(SCADA_PORT))
 while 1:
 	NewClientSock = ClientSock.accept()[0]
-	newthread = ClientThread(NewClientSock) 
-	client_threads_collection.append(newthread)
-	newthread.daemon = True  ##to znaczy ze watek zostanie zamkniety/umrze jak zginie watek glowny
-	newthread.start()
-	#NewClientSock.close() # dunno 
-
+	newThread = ClientThread(NewClientSock) 
+	clientThreadsCollection.append(newThread)
+	newThread.daemon = True
+	newThread.start()
